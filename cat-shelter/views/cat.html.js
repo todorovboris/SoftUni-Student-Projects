fa25bfd6144bd51c0a75d2c4aeac38d24297479a
Boris Todorov <btodorovtodorov@gmail.com>
2025-01-10 19:54:22 +0200

    cat-shelter project transfer

---
+++ cat-shelter/views/cat.html.js
@@ -0,0 +1,11 @@
+export default (cat) => `                  
+<li>
+    <img src="${cat.imageUrl}" alt="${cat.name}" />
+    <h3>${cat.name}</h3>
+    <p><span>Breed: </span>${cat.breed}</p>
+    <p><span>Description: </span>${cat.description}</p>
+    <ul class="buttons">
+        <li class="btn edit"><a href="">Change Info</a></li>
+        <li class="btn delete"><a href="">New Home</a></li>
+    </ul>
+</li>`;
