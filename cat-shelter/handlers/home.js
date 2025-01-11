a24687a3445eba03eb155206ea195352a3f0c871
Boris Todorov <btodorovtodorov@gmail.com>
2025-01-11 18:04:10 +0200

    home handler added

--- cat-shelter/handlers/home.js
+++ cat-shelter/handlers/home.js
@@ -1,3 +1,16 @@
 import url from 'url';
 import fs from 'fs';
 import path from 'path';
+
+import cats from '../data/catsDatabase.json';
+import breeds from '../data/breedsDatabase.json';
+
+export default (req, res) => {
+    const pathname = url.parse(req.url).pathname;
+
+    if (pathname === '/' && req.method === 'GET') {
+        let filePath = path.normalize(path.join(__dirname, '../views/home/index.html'));
+    } else {
+        return true;
+    }
+};
