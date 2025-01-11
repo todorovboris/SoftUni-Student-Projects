bea66ecb48d5656a57c6a42e9fd2434abcf37c36
Boris Todorov <btodorovtodorov@gmail.com>
2025-01-11 14:45:58 +0200

    show cats from json

--- cat-shelter/index.js
+++ cat-shelter/index.js
@@ -1,4 +1,6 @@
 import http from 'http';
+import { v4 as uuid } from 'uuid';
+import fs from 'fs/promises';
 
 import mainCss from './content/styles/site.css.js';
 
@@ -6,43 +8,9 @@ import homePage from './views/home/index.html.js';
 import addBreedPage from './views/addBreed.html.js';
 import addCatPage from './views/addCat.html.js';
 
-const cats = [
-    {
-        id: 1,
-        imageUrl: 'https://ichef.bbci.co.uk/news/976/cpsprodpb/12A9B/production/_111434467_gettyimages-1143489763.jpg',
-        name: 'Choco',
-        breed: 'Bombay Cat',
-        description: 'Dominant and aggressive to other cats. Will probably eat you in your sleep. Very cute tho.',
-    },
-    {
-        id: 2,
-        imageUrl: 'https://cdn.pixabay.com/photo/2015/06/19/14/20/cat-814952_1280.jpg',
-        name: 'Mocko',
-        breed: 'Persian',
-        description: 'Dominant and aggressive to other cats. Will probably eat you in your sleep. Very cute tho.',
-    },
-    {
-        id: 3,
-        imageUrl: 'https://cdn.pixabay.com/photo/2018/08/08/05/12/cat-3591348_1280.jpg',
-        name: 'Roshko',
-        breed: 'Siamse Cat',
-        description: 'Dominant and aggressive to other cats. Will probably eat you in your sleep. Very cute tho.',
-    },
-    {
-        id: 4,
-        imageUrl: 'https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg',
-        name: 'Lucky',
-        breed: 'Black Cat',
-        description: 'Dominant and aggressive to other cats. Will probably eat you in your sleep. Very cute tho.',
-    },
-    {
-        id: 5,
-        imageUrl: 'https://cdn.pixabay.com/photo/2014/04/13/20/49/cat-323262_1280.jpg',
-        name: 'Mats',
-        breed: 'White Cat',
-        description: 'Dominant and aggressive to other cats. Will probably eat you in your sleep. Very cute tho.',
-    },
-];
+let cats = [];
+
+showCatsFromJson();
 
 const server = http.createServer((req, res) => {
     // POST Request
@@ -57,7 +25,7 @@ const server = http.createServer((req, res) => {
             const data = new URLSearchParams(body);
 
             cats.push({
-                id: cats.length + 1,
+                id: uuid(),
                 ...Object.fromEntries(data.entries()),
             });
 
@@ -116,6 +84,16 @@ const server = http.createServer((req, res) => {
     res.write(responseBody);
     res.end();
 });
+
+async function showCatsFromJson() {
+    // read cats
+    try {
+        const catsJson = await fs.readFile('./catsDatabase.json', { encoding: 'utf-8' });
+        cats = JSON.parse(catsJson);
+    } catch (err) {
+        console.log(err.message);
+    }
+}
 
 server.listen(4000);
 console.log('Server is listening on http://localhost:4000...');
