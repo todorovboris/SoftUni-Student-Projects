a7ae461b81954b1483d82e1824f63f9d2909c360
Boris Todorov <btodorovtodorov@gmail.com>
2025-01-11 19:04:46 +0200

    Update home.js

--- cat-shelter/handlers/home.js
+++ cat-shelter/handlers/home.js
@@ -10,6 +10,20 @@ export default (req, res) => {
 
     if (pathname === '/' && req.method === 'GET') {
         let filePath = path.normalize(path.join(__dirname, '../views/home/index.html'));
+
+        fs.readFile(filePath, (err, data) => {
+            if (err) {
+                console.log(err);
+
+                res.writeHead(404, { 'Content-Type': 'text/plain' });
+                res.write('404 Not Found');
+                res.end();
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.write(data);
+                res.end();
+            }
+        });
     } else {
         return true;
     }
