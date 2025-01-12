f1f0e2bca417d5f3bc25e6039613224e9b0461de
Boris Todorov <btodorovtodorov@gmail.com>
2025-01-12 16:11:26 +0200

    Create static.js

---
+++ cat-shelter copy/handlers/static.js
@@ -0,0 +1,38 @@
+import url from 'url';
+import fs from 'fs';
+import path from 'path';
+
+function getContentType(url) {
+    if (url.endsWith('css')) {
+        return 'text/css';
+    } else if ('TODO...') {
+        // TODO...
+    }
+}
+
+export const staticFiles = (req, res) => {
+    const pathname = url.parse(req.url).pathname;
+
+    if (pathname.startsWith('/content') && req.method === 'GET') {
+        fs.readFile(`./${pathname}`, 'utf-8', (err, data) => {
+            if (err) {
+                console.log(err);
+
+                res.writeHead(404, {
+                    'content-type': 'text/plain',
+                });
+                res.write('Error was found!');
+                res.end();
+                return;
+            }
+
+            console.log(pathname);
+
+            res.writeHead(200, { 'content-type': getContentType(pathname) });
+            res.write(data);
+            res.end();
+        });
+    } else {
+        return true;
+    }
+};
