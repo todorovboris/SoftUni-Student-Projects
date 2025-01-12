040479c0f3fc9d507cd3df443cadd9229d9747dd
Boris Todorov <btodorovtodorov@gmail.com>
2025-01-12 16:35:11 +0200

    Create addCat.js

---
+++ cat-shelter copy/handlers/addCat.js
@@ -0,0 +1,34 @@
+import url from 'url';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const cats = JSON.parse(fs.readFileSync('./data/cats.json', 'utf-8'));
+const breeds = JSON.parse(fs.readFileSync('./data/breeds.json', 'utf-8'));
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export const homeHandler = (req, res) => {
+    const pathname = url.parse(req.url).pathname;
+
+    // showing ADD CAT html
+    if (pathname === '/cats/add-cat' && req.method === 'GET') {
+        let filePath = path.normalize(path.join(__dirname, '../views/addCat.html'));
+
+        fs.readFile(filePath, (err, data) => {
+            if (err) {
+                res.writeHead(404, { 'Content-Type': 'text/plain' });
+                res.write('404 Not Found');
+                res.end();
+                return;
+            }
+
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write(data);
+            res.end();
+        });
+    } else {
+        return true;
+    }
+};
