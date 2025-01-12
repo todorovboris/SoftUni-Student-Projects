3dc0bdd89f8ae2d5710428fe870561d5066bf17d
Boris Todorov <btodorovtodorov@gmail.com>
2025-01-12 16:41:04 +0200

    add catHandler

---
+++ cat-shelter copy/handlers/cats.js
@@ -0,0 +1,49 @@
+import url from 'url';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const cats = JSON.parse(fs.readFileSync('./data/cats.json', 'utf-8'));
+const breeds = JSON.parse(fs.readFileSync('./data/breeds.json', 'utf-8'));
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export const catHandler = (req, res) => {
+    const pathname = url.parse(req.url).pathname;
+
+    // showing HOME html
+    if (pathname === '/cats/add-cat' && req.method === 'GET') {
+        let filePath = path.normalize(path.join(__dirname, '../views/addCat.html'));
+
+        fs.readFile(filePath, (err, data) => {
+            if (err) {
+                res.writeHead(404, { 'Content-Type': 'text/plain' });
+                res.write('404 Not Found');
+                res.end();
+                return;
+            }
+
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write(data);
+            res.end();
+        });
+    } else if (pathname === '/cats/add-breed' && req.method === 'GET') {
+        let filePath = path.normalize(path.join(__dirname, '../views/addBreed.html'));
+
+        fs.readFile(filePath, (err, data) => {
+            if (err) {
+                res.writeHead(404, { 'Content-Type': 'text/plain' });
+                res.write('404 Not Found');
+                res.end();
+                return;
+            }
+
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write(data);
+            res.end();
+        });
+    } else {
+        return true;
+    }
+};
--- cat-shelter copy/handlers/index.js
+++ cat-shelter copy/handlers/index.js
@@ -1,4 +1,5 @@
 import { homeHandler } from './home.js';
 import { staticFiles } from './static.js';
+import { catHandler } from './cats.js';
 
-export default [homeHandler, staticFiles];
+export default [homeHandler, staticFiles, catHandler];
