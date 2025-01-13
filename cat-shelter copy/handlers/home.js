bf4dad1d6a36f4c50112d15e18f08f28bffae457
Boris Todorov <btodorovtodorov@gmail.com>
2025-01-13 16:05:49 +0200

    render home page

--- cat-shelter copy/handlers/home.js
+++ cat-shelter copy/handlers/home.js
@@ -1,33 +1,46 @@
 import url from 'url';
-import fs from 'fs';
+import fs from 'fs/promises';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const cats = JSON.parse(fs.readFileSync('./data/cats.json', 'utf-8'));
-const breeds = JSON.parse(fs.readFileSync('./data/breeds.json', 'utf-8'));
+const catsPath = path.resolve(__dirname, '../data/cats.json');
+const cats = JSON.parse(await fs.readFile(catsPath, 'utf-8'));
 
-export const homeHandler = (req, res) => {
+export const homeHandler = async (req, res) => {
     const pathname = url.parse(req.url).pathname;
 
     // showing HOME html
     if (pathname === '/' && req.method === 'GET') {
-        let filePath = path.normalize(path.join(__dirname, '../views/home/index.html'));
+        try {
+            const filePath = path.normalize(path.join(__dirname, '../views/home/index.html'));
+            const data = await fs.readFile(filePath, { encoding: 'utf-8' });
 
-        fs.readFile(filePath, (err, data) => {
-            if (err) {
-                res.writeHead(404, { 'Content-Type': 'text/plain' });
-                res.write('404 Not Found');
-                res.end();
-                return;
-            }
+            const catsTemplate = cats.map(
+                (cat) => `<li>
+                        <img src="${cat.imageUrl}" alt="${cat.name}" />
+                        <h3>${cat.name}</h3>
+                        <p><span>Breed: </span>${cat.breed}</p>
+                        <p><span>Description: </span>${cat.description}</p>
+                        <ul class="buttons">
+                            <li class="btn edit"><a href="">Change Info</a></li>
+                            <li class="btn delete"><a href="">New Home</a></li>
+                        </ul>
+                    </li>`
+            );
+
+            let modifiedData = data.toString().replace('{{cats}}', catsTemplate);
 
             res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.write(data);
+            res.write(modifiedData);
             res.end();
-        });
+        } catch (err) {
+            res.writeHead(404, { 'Content-Type': 'text/plain' });
+            res.write('404 Not Found');
+            res.end();
+        }
     } else {
         return true;
     }
