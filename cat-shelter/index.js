24e995e3b3dfe5b8e7767b2ea417b1d716db8762
Boris Todorov <btodorovtodorov@gmail.com>
2025-01-14 21:59:41 +0200

    Update index.js

--- cat-shelter/index.js
+++ cat-shelter/index.js
@@ -5,6 +5,7 @@ app.listen(5001, () => console.log('Server is listening on http://localhost:5001
 
 app.get('/', homePageHandler);
 app.get('/search', searchPageHandler);
+app.get('/user/:userId', userIdPage);
 app.get('*', notfound); // всичко, което НЕ влиза в горните пътища
 
 function homePageHandler(req, res) {
@@ -13,6 +14,11 @@ function homePageHandler(req, res) {
 
 function searchPageHandler(req, res) {
     res.send('<h3>Search Page</h3>');
+}
+
+function userIdPage(req, res) {
+    const paramsObj = req.params;
+    res.send(paramsObj); // връща обект с ключ името на параметъра и стойност = рикуестнатата
 }
 
 function notfound(req, res) {
