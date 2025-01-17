df8919fa794f3913791d0a6973bcf43999f05e92
Boris Todorov <btodorovtodorov@gmail.com>
2025-01-17 19:01:30 +0200

    add handlebars

--- movie-magic/src/index.js
+++ movie-magic/src/index.js
@@ -1,8 +1,13 @@
 import express from 'express';
+import handlebars from 'express-handlebars';
 
 const app = express();
 app.listen(5001, () => console.log('Server is listening on http://localhost:5001...'));
 
+app.engine('hbs', handlebars.engine({ extname: 'hbs' }));
+app.set('view engine', 'hbs');
+app.set('views', './src/views');
+
 app.get('/', (req, res) => {
-    res.send('It Works!');
+    res.render('home', { layout: false });
 });
