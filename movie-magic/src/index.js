075f868cf19f0cf26cacf81451a294267b05dc24
Boris Todorov <btodorovtodorov@gmail.com>
2025-01-17 19:14:46 +0200

    show home page

--- movie-magic/src/index.js
+++ movie-magic/src/index.js
@@ -7,6 +7,7 @@ app.listen(5001, () => console.log('Server is listening on http://localhost:5001
 app.engine('hbs', handlebars.engine({ extname: 'hbs' }));
 app.set('view engine', 'hbs');
 app.set('views', './src/views');
+app.use('/static', express.static('src/public'));
 
 app.get('/', (req, res) => {
     res.render('home', { layout: false });
