ad1d74b5d43e492da826aae97319919e949641bf
Boris Todorov <btodorovtodorov@gmail.com>
2025-01-17 18:47:24 +0200

    add resourses

--- movie-magic/src/index.js
+++ movie-magic/src/index.js
@@ -1,1 +1,8 @@
-console.log('It Works!');
+import express from 'express';
+
+const app = express();
+app.listen(5001, () => console.log('Server is listening on http://localhost:5001...'));
+
+app.get('/', (req, res) => {
+    res.send('It Works!');
+});
