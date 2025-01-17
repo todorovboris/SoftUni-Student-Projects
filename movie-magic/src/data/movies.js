5abb8bb67d0539abb59ba0dc9fd422cfbd282cbb
Boris Todorov <btodorovtodorov@gmail.com>
2025-01-17 21:35:56 +0200

    update details page

--- movie-magic/src/data/movies.js
+++ movie-magic/src/data/movies.js
@@ -6,6 +6,10 @@ export default [
         genre: 'Adventure',
         description:
             'Dreaming about saving countless lives and having another adventure, the feisty English feminist and doctor of botany, Dr Lily Houghton, embarks on a peril-laden mission to change the world. Along with her fashionable brother, MacGregor, Dr Houghton enlists the help of the arrogant, wisecracking riverboat skipper, Captain Frank Wolff, to guide them through the serpentine Amazon River in La Quila, his swift wooden boat. Now, as the intrepid trio ventures deeper and deeper into the heart of an impenetrable green maze, searching for something that cannot be found, a centuries-old curse and the ruthless aristocrat, Prince Joachim, threaten to put an end to their ambitious plans.',
+        directror: 'Rob Marshall',
+        year: 2000,
+        rating: 10,
+        category: 'Movie',
     },
     {
         id: 2,
@@ -14,6 +18,10 @@ export default [
         genre: 'Fantasy',
         description:
             "The youngest of King Triton's daughters, Ariel is a beautiful and spirited young mermaid with a thirst for adventure. Longing to find out more about the world beyond the sea, Ariel visits the surface and falls for the dashing Prince Eric. Following her heart, she makes a deal with the evil sea witch, Ursula, to experience life on land.",
+        directror: '',
+        year: 2000,
+        rating: 10,
+        category: '',
     },
     {
         id: 3,
