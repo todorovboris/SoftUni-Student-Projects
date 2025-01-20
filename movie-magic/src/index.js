5e20f229b1aecd7134ae0675655b6d8c574c35a7
Boris Todorov <btodorovtodorov@gmail.com>
2025-01-20 13:16:00 +0200

    parse form data on server

--- movie-magic/src/index.js
+++ movie-magic/src/index.js
@@ -4,12 +4,13 @@ import handlebars from 'express-handlebars';
 import routes from './routes.js';
 
 const app = express();
-app.listen(5001, () => console.log('Server is listening on http://localhost:5001...'));
+app.engine('hbs', handlebars.engine({ extname: 'hbs' })); // what are the extensions of pages and { layouts }
 
-app.engine('hbs', handlebars.engine({ extname: 'hbs' }));
+app.set('view engine', 'hbs'); // set the default engine of the app
+app.set('views', './src/views'); // set-up where to search the template pages
 
-app.set('view engine', 'hbs');
-app.set('views', './src/views');
+app.use('/static', express.static('src/public')); // learn express where to search for the static files(images, css, etc...)
+app.use(express.urlencoded({ extended: false })); // learn express to parse form data
 
-app.use('/static', express.static('src/public'));
 app.use(routes);
+app.listen(5001, () => console.log('Server is listening on http://localhost:5001...'));
