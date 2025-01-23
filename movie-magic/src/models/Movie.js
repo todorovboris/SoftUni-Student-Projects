eb73054bb337c6fb64a77b064b501fab307dd17e
Boris Todorov <btodorovtodorov@gmail.com>
2025-01-23 21:33:26 +0200

    Update Movie.js

--- movie-magic/src/models/Movie.js
+++ movie-magic/src/models/Movie.js
@@ -7,7 +7,7 @@ const movieSchema = new Schema({
     genre: String,
     director: String,
     year: Number,
-    imageURL: String,
+    imageUrl: String,
     rating: Number,
     description: String,
 });
