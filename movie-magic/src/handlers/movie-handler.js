c33290eea48922c24cef1afae3017d1fe1b9e961
Boris Todorov <btodorovtodorov@gmail.com>
2025-01-27 13:00:30 +0200

    modify attaching casts to movies - second way

--- movie-magic/src/handlers/movie-handler.js
+++ movie-magic/src/handlers/movie-handler.js
@@ -34,12 +34,13 @@ export default {
         return newMoviePromise;
     },
     async attachCast(movieId, castId) {
-        // Attach #1
-        const movie = await Movie.findById(movieId);
-        movie.casts.push(castId);
-        await movie.save();
+        // *Attach #1
+        // const movie = await Movie.findById(movieId);
+        // movie.casts.push(castId);
+        // await movie.save();
+        // return movie;
 
-        return movie;
-        // Attach #2
+        // *Attach #2
+        return Movie.findByIdAndUpdate(movieId, { $push: { casts: castId } }); //*mongoDb
     },
 };
