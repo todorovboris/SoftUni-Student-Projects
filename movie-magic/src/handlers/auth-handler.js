06491049c2cda543a860e55afbc7e459627cded7
Boris Todorov <btodorovtodorov@gmail.com>
2025-02-04 13:49:06 +0200

    hash the password on save

--- movie-magic/src/handlers/auth-handler.js
+++ movie-magic/src/handlers/auth-handler.js
@@ -1,9 +1,15 @@
+import bcrypt from 'bcrypt';
+
 import User from '../models/User.js';
 
 export default {
     async register(userData) {
-        const newRegistration = User.create({ ...userData });
+        //! if hash the password in the handler
+        // const { email, password } = userData;
+        // const salt = await bcrypt.genSalt(10);
+        // const hashEmail = await bcrypt.hash(email, salt);
+        // const hashPass = await bcrypt.hash(password, salt);
 
-        return newRegistration;
+        return User.create({ ...userData });
     },
 };
--- movie-magic/src/models/User.js
+++ movie-magic/src/models/User.js
@@ -1,8 +1,14 @@
+import bcrypt from 'bcrypt';
 import { Schema, model } from 'mongoose';
 
 const userSchema = new Schema({
     email: String,
     password: String,
+});
+
+userSchema.pre('save', async function () {
+    // TODO: fix update user big
+    this.password = await bcrypt.hash(this.password, 10);
 });
 
 const User = model('User', userSchema);
