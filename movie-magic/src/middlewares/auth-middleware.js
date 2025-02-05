d8e64240d1841cdc9a52ba725737677f11ddb7fb
Boris Todorov <btodorovtodorov@gmail.com>
2025-02-05 15:54:45 +0200

    clear cookies if you have invalid token

--- movie-magic/src/middlewares/auth-middleware.js
+++ movie-magic/src/middlewares/auth-middleware.js
@@ -18,5 +18,7 @@ export const authMiddleware = (req, res, next) => {
         next();
     } catch (err) {
         // TODO: Invalid token
+        res.clearCookie('auth');
+        res.redirect('/auth/login');
     }
 };
