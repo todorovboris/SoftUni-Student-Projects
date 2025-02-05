eb4eb949d636cf9bf72da172201defab99362dc2
Boris Todorov <btodorovtodorov@gmail.com>
2025-02-05 11:40:18 +0200

    add login functionality with jwt token generation

--- movie-magic/src/controllers/auth-controller.js
+++ movie-magic/src/controllers/auth-controller.js
@@ -10,13 +10,32 @@ authController.get('/register', (req, res) => {
 authController.post('/register', async (req, res) => {
     const userData = req.body;
 
-    await authHandler.register(userData);
+    try {
+        await authHandler.register(userData);
+    } catch (err) {
+        console.log(err.message);
+        return res.redirect('/404');
+    }
 
     res.redirect('/auth/login');
 });
 
 authController.get('/login', (req, res) => {
     res.render('auth/login');
+});
+
+authController.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const token = await authHandler.login(email, password);
+        console.log(token);
+    } catch (err) {
+        console.log(err.message);
+        return res.redirect('/404');
+    }
+
+    res.redirect('/');
 });
 
 export default authController;
--- movie-magic/src/handlers/auth-handler.js
+++ movie-magic/src/handlers/auth-handler.js
@@ -1,6 +1,7 @@
-import bcrypt from 'bcrypt';
-
 import User from '../models/User.js';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import 'dotenv/config';
 
 export default {
     async register(userData) {
@@ -11,5 +12,29 @@ export default {
         // const hashPass = await bcrypt.hash(password, salt);
 
         return User.create({ ...userData });
+    },
+    async login(email, password) {
+        const user = await User.findOne({ email });
+
+        //* check if user exists
+        if (!user) {
+            throw new Error('Invalid email or password!');
+        }
+
+        //* check if password is correct
+        const isValid = await bcrypt.compare(password, user.password);
+        if (!isValid) {
+            throw new Error('Invalid email or password!');
+        }
+
+        //* generate token
+        const payload = {
+            _id: user._id,
+            email: user.email,
+        };
+        const token = jwt.sign(payload, process.env.SECRET, { expiresIn: '2h' });
+
+        //* return token
+        return token;
     },
 };
