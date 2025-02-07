1e49d5d573726c9037f8fdedd1a65a2d5f5d7091
Boris Todorov <btodorovtodorov@gmail.com>
2025-02-07 14:28:48 +0200

    add Cast Model validation

--- movie-magic/src/models/Cast.js
+++ movie-magic/src/models/Cast.js
@@ -1,10 +1,27 @@
 import { Schema, model } from 'mongoose';
 
 const castSchema = new Schema({
-    name: String,
-    age: Number,
-    born: String,
-    imageUrl: String,
+    name: {
+        type: String,
+        required: [true, 'Name is required!'],
+        minLength: [5, 'Name should be at least 5 characters long!'],
+        match: [/^[a-zA-Z0-9 ]+$/, 'Name should includes alphanumeric, digits and whitespaces symbols only!'],
+    },
+    age: {
+        type: Number,
+        min: 1,
+        max: 120,
+    },
+    born: {
+        type: String,
+        required: [true, 'Born is required!'],
+        minLength: [10, 'Born should be at least 5 characters long!'],
+        match: [/^[a-zA-Z0-9 ]+$/, 'Born should includes alphanumeric, digits and whitespaces symbols only!'],
+    },
+    imageUrl: {
+        type: String,
+        match: [/^https?:\/\//, 'Image URL should start with http:// or https://...'],
+    },
 });
 
 const Cast = model('Cast', castSchema);
