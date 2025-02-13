2d108dae4ddbc7ade8d32c5be46464759a2ac31f
Boris Todorov <btodorovtodorov@gmail.com>
2025-02-13 18:40:11 +0200

    add search page

--- js-back-end-exam-prep/03_MagmaHaven/src/controllers/home-controller.js
+++ js-back-end-exam-prep/03_MagmaHaven/src/controllers/home-controller.js
@@ -6,4 +6,8 @@ homeController.get('/', (req, res) => {
     res.render('home', { pageTitle: 'Home' });
 });
 
+homeController.get('/search', (req, res) => {
+    res.render('search', { pageTitle: 'Search' });
+});
+
 export default homeController;
