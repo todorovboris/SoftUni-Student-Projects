9c797c0a31b68cb0ba12a2d66349dd7ea9d466f2
Boris Todorov <btodorovtodorov@gmail.com>
2025-02-13 15:36:53 +0200

    add validations for the Models

--- js-back-end-exam-prep/02-HomeRecipies/src/models/Recipe.js
+++ js-back-end-exam-prep/02-HomeRecipies/src/models/Recipe.js
@@ -5,22 +5,29 @@ const recipeSchema = new Schema(
         title: {
             type: String,
             required: [true, 'Title is required!'],
+            minLength: [2, 'Title should be at least 2 characters long!'],
         },
         description: {
             type: String,
             required: [true, 'Description is required!'],
+            minLength: [10, 'Description should be at least 10 characters long!'],
+            maxLength: [100, 'Description should be maximum 100 characters long!'],
         },
         ingredients: {
             type: String,
             required: [true, 'Ingredients is required!'],
+            minLength: [10, 'Ingredients should be at least 10 characters long!'],
+            maxLength: [200, 'Ingredients should be maximum 200 characters long!'],
         },
         instructions: {
             type: String,
             required: [true, 'Instructions is required!'],
+            minLength: [10, 'Instructions should be at least 2 characters long!'],
         },
         image: {
             type: String,
             required: [true, 'Image is required!'],
+            match: [/^https?:\/\//, 'Image URL should start with http:// or https://...'],
         },
         recommendList: [
             {
--- js-back-end-exam-prep/02-HomeRecipies/src/models/User.js
+++ js-back-end-exam-prep/02-HomeRecipies/src/models/User.js
@@ -6,14 +6,18 @@ const userSchema = new Schema({
     name: {
         type: String,
         required: [true, 'Name is required!'],
+        minLength: [2, 'Name should be at least 2 characters long!'],
+        maxLength: [20, 'Name should be maximum 20 characters long!'],
     },
     email: {
         type: String,
         required: [true, 'Email is required!'],
+        minLength: [10, 'Email should be at least 10 characters long!'],
     },
     password: {
         type: String,
         required: [true, 'Password is required!'],
+        minLength: [4, 'Password should be at least 4 characters long!'],
     },
 });
 
