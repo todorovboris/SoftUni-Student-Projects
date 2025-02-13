561cd0edf71343b5a6626386fe6d20bb2de3f452
Boris Todorov <btodorovtodorov@gmail.com>
2025-02-13 19:56:02 +0200

    add validations to the Models

--- js-back-end-exam-prep/03_MagmaHaven/src/models/Volcano.js
+++ js-back-end-exam-prep/03_MagmaHaven/src/models/Volcano.js
@@ -5,22 +5,28 @@ const volcanoSchema = new Schema(
         name: {
             type: String,
             required: [true, 'Name is required!'],
+            minLength: [2, 'Name should be at least 2 characters long!'],
         },
         location: {
             type: String,
             required: [true, 'Location is required!'],
+            minLength: [3, 'Location should be at least 3 characters long!'],
         },
         elevation: {
             type: Number,
             required: [true, 'Elevation is required!'],
+            min: [0, 'Elevation should be minimum 0!'],
         },
         lastEruption: {
             type: Number,
             required: [true, 'Last eruption is required!'],
+            min: [0, 'The Year of Last Eruption should be between 0 and 2024!'],
+            max: [2024, 'The Year of Last Eruption should be between 0 and 2024!'],
         },
         image: {
             type: String,
             required: [true, 'Image is required!'],
+            match: [/^https?:\/\//, 'Image URL should start with http:// or https://...'],
         },
         typeVolcano: {
             type: String,
@@ -30,6 +36,7 @@ const volcanoSchema = new Schema(
         description: {
             type: String,
             required: [true, 'Description is required!'],
+            minLength: [10, 'Username should be at least 10 characters long!'],
         },
         voteList: [
             {
