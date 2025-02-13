38411bd4b4cd9b809696cec764f869a71045d619
Boris Todorov <btodorovtodorov@gmail.com>
2025-02-13 16:30:22 +0200

    initial installation of 3rd exam prep project

---
+++ js-back-end-exam-prep/03_MagmaHaven/src/utils/auth-utils.js
@@ -0,0 +1,10 @@
+import jwt from 'jsonwebtoken';
+import { JWT_SECRET } from '../config.js';
+
+export const generateToken = (user) => {
+    const payload = { _id: user._id, email: user.email, name: user.name };
+    const secretKey = JWT_SECRET;
+    const token = jwt.sign(payload, secretKey, { expiresIn: '2h' });
+
+    return token;
+};
