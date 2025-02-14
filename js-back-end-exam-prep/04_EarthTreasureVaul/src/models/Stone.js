6a43f32e6f18fa3ea5f5698631e53e832c2df09b
Boris Todorov <btodorovtodorov@gmail.com>
2025-02-14 17:00:19 +0200

    add Model validations

--- js-back-end-exam-prep/04_EarthTreasureVaul/src/models/Stone.js
+++ js-back-end-exam-prep/04_EarthTreasureVaul/src/models/Stone.js
@@ -5,30 +5,39 @@ const stoneSchema = new Schema(
         name: {
             type: String,
             required: [true, 'Name is required!'],
+            minLength: [2, 'Name should be at least 2 characters long'],
         },
         category: {
             type: String,
             required: [true, 'Category is required!'],
+            minLength: [3, 'Category should be at least 3 characters long'],
         },
         color: {
             type: String,
             required: [true, 'Color is required!'],
+            minLength: [2, 'Color should be at least 2 characters long'],
         },
         image: {
             type: String,
             required: [true, 'Image is required!'],
+            match: [/^https?:\/\//, 'Image URL should start with http:// or https://...'],
         },
         location: {
             type: String,
             required: [true, 'Location is required!'],
+            minLength: [5, 'Location should be at least 5 characters long'],
+            maxLength: [15, 'Location should be at maximum 15 characters long'],
         },
         formula: {
             type: String,
             required: [true, 'Formula is required!'],
+            minLength: [3, 'Formula should be at least 3 characters long'],
+            maxLength: [30, 'Formula should be at maximum 30 characters long'],
         },
         description: {
             type: String,
             required: [true, 'Description is required!'],
+            minLength: [10, 'Description should be at least 10 characters long'],
         },
         likedList: [
             {
--- js-back-end-exam-prep/04_EarthTreasureVaul/src/models/User.js
+++ js-back-end-exam-prep/04_EarthTreasureVaul/src/models/User.js
@@ -6,10 +6,12 @@ const userSchema = new Schema({
     email: {
         type: String,
         required: [true, 'Email is required!'],
+        minLength: [10, 'Email should be at least 10 characters long'],
     },
     password: {
         type: String,
         required: [true, 'Password is required!'],
+        minLength: [4, 'Password should be at least 4 characters long'],
     },
 });
 
