dacb75a4bb16be301922c740b24f2eddd5bce68d
Boris Todorov <btodorovtodorov@gmail.com>
2025-02-14 15:34:37 +0200

    update home page

--- js-back-end-exam-prep/04_EarthTreasureVaul/src/controllers/home-controller.js
+++ js-back-end-exam-prep/04_EarthTreasureVaul/src/controllers/home-controller.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
+import stoneHandler from '../handlers/stone-handler.js';
 
 const homeController = Router();
 
-homeController.get('/', (req, res) => {
-    res.render('home', { pageTitle: 'Home' });
+homeController.get('/', async (req, res) => {
+    const stones = await stoneHandler.getLatestStones();
+    res.render('home', { stones, pageTitle: 'Home' });
 });
 
 export default homeController;
--- js-back-end-exam-prep/04_EarthTreasureVaul/src/handlers/stone-handler.js
+++ js-back-end-exam-prep/04_EarthTreasureVaul/src/handlers/stone-handler.js
@@ -1,6 +1,9 @@
 import Stone from '../models/Stone.js';
 
 export default {
+    getLatestStones() {
+        return Stone.find({}).sort({ _id: 'desc' }).limit(3);
+    },
     createStone(stoneData, userId) {
         return Stone.create({ ...stoneData, owner: userId });
     },
