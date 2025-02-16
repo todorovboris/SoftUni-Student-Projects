b8fe9a70a9ad9b73500ec7f1b441831e2fbc8712
Boris Todorov <btodorovtodorov@gmail.com>
2025-02-16 11:19:15 +0200

    add validations for Models

--- js-back-end-exam-prep/99_FinalExam/src/models/Disaster.js
+++ js-back-end-exam-prep/99_FinalExam/src/models/Disaster.js
@@ -5,6 +5,7 @@ const disasterSchema = new Schema(
         name: {
             type: String,
             required: [true, 'Name is required!'],
+            minLength: [2, 'Name should be at least 2 characters long!'],
         },
         type: {
             type: String,
@@ -14,18 +15,23 @@ const disasterSchema = new Schema(
         year: {
             type: Number,
             required: [true, 'Year is required!'],
+            min: [0, 'The Year should be between 0 and 2024!'],
+            max: [2024, 'The Year should be between 0 and 2024!'],
         },
         location: {
             type: String,
             required: [true, 'Location is required!'],
+            minLength: [3, 'Location should be at least 3 characters long!'],
         },
         image: {
             type: String,
             required: [true, 'Image is required!'],
+            match: [/^https?:\/\//, 'Image URL should start with http:// or https://...'],
         },
         description: {
             type: String,
             required: [true, 'Description is required!'],
+            minLength: [10, 'Description should be at least 10 characters long!'],
         },
         interestedList: [
             {
--- js-back-end-exam-prep/99_FinalExam/src/models/User.js
+++ js-back-end-exam-prep/99_FinalExam/src/models/User.js
@@ -6,14 +6,17 @@ const userSchema = new Schema({
     name: {
         type: String,
         required: [true, 'Username is required!'],
+        minLength: [2, 'Username should be at least 2 characters long!'],
     },
     email: {
         type: String,
         required: [true, 'Email is required!'],
+        minLength: [10, 'Email should be at least 10 characters long!'],
     },
     password: {
         type: String,
         required: [true, 'Password is required!'],
+        minLength: [4, 'Password should be at least 4 characters long!'],
     },
 });
 
