ea80249cabc256758408e8bf72cbbbd293eef3b8
Boris Todorov <btodorovtodorov@gmail.com>
2025-03-06 19:40:34 +0200

    fix post data

--- user-list/client/src/services/userService.js
+++ user-list/client/src/services/userService.js
@@ -9,6 +9,12 @@ export default {
         return users;
     },
     async create(userData) {
+        const { country, city, street, streetNumber, postData } = userData;
+
+        postData.address = { country, city, street, streetNumber };
+        postData.createdAt = new Date().toISOString;
+        postData.updatedAt = new Date().toISOString;
+
         const response = await fetch(baseUrl, {
             method: 'POST',
             headers: {
