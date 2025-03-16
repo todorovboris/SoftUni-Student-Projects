92969d85f954bbc80e1e053dc5bca7706299f456
Boris Todorov <btodorovtodorov@gmail.com>
2025-03-16 20:14:24 +0200

    save comment to the server

--- react-gamesplay/client/src/components/comments-create/CommentsCreate.jsx
+++ react-gamesplay/client/src/components/comments-create/CommentsCreate.jsx
@@ -1,9 +1,11 @@
-export default function CommentsCreate({ email }) {
-    const commentAction = (formData) => {
+import commentService from '../../services/commentService.js';
+
+export default function CommentsCreate({ email, gameId }) {
+    const commentAction = async (formData) => {
         const comment = formData.get('comment');
 
-        console.log(email);
-        console.log(comment);
+        const createdComment = await commentService.create(email, gameId, comment);
+        console.log(createdComment);
     };
     return (
         <article className="create-comment">
