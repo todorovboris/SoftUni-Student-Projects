d1d286d714a388542f4721b08ff36bf58e1ea1fd
Boris Todorov <btodorovtodorov@gmail.com>
2025-03-19 23:11:09 +0200

    Update request.js

--- react-gamesplay/client/src/utils/request.js
+++ react-gamesplay/client/src/utils/request.js
@@ -27,37 +27,3 @@ export default {
     put: (url, data, options) => request('PUT', url, data, options),
     delete: (url, options) => request('DELETE', url, null, options),
 };
-
-// const api = {
-//     get: (url, options) => request('GET', url, null, options),
-//     post: (url, data, options) => request('POST', url, data, options),
-//     put: (url, data, options) => request('PUT', url, data, options),
-//     delete: (url, options) => request('DELETE', url, null, options),
-// };
-
-// const request = async (method, url, data, options = {}) => {
-//     if (method != 'GET') {
-//         options.method = method;
-//     }
-
-//     if (data) {
-//         options = {
-//             ...options,
-//             headers: {
-//                 'Content-Type': 'application/json',
-//             },
-//             body: JSON.stringify(data),
-//         };
-//     }
-
-//     const response = await fetch(url, options);
-//     const result = await response.json();
-//     return result;
-// };
-
-// export default {
-//     get: request.bind(null, 'GET'),
-//     post: request.bind(null, 'POST'),
-//     put: request.bind(null, 'PUT'),
-//     delete: request.bind(null, 'DELETE'),
-// };
