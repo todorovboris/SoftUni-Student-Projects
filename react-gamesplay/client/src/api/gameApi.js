2b5b21624b158970c53a2314d502b9cf5e16b369
Boris Todorov <btodorovtodorov@gmail.com>
2025-03-19 22:44:45 +0200

    select properties on search

--- react-gamesplay/client/src/api/gameApi.js
+++ react-gamesplay/client/src/api/gameApi.js
@@ -32,6 +32,7 @@ export const useLatestGames = () => {
         const searchParams = new URLSearchParams({
             sortBy: '_createdOn desc',
             pageSize: 3,
+            select: '_id,imageUrl,title',
         });
 
         request.get(`${baseUrl}?${searchParams.toString()}`).then(setLatestGames);
