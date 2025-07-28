83c0af2195ee7ede5fe7c9b6c81e292afea8ac9d
Boris Todorov <btodorovtodorov@gmail.com>
2025-07-28 15:41:00 +0300

    create posts and themes services

---
+++ angular-softuni-forum/client/src/app/services/posts.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PostsService {
+  constructor() {}
+
+  getPosts(limit?: number) {
+    //
+  }
+}
