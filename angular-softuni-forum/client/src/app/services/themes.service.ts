4d6434d82487807985d566824042d1f6560f84f3
Boris Todorov <btodorovtodorov@gmail.com>
2025-07-28 16:58:33 +0300

    create themes service file for api operations

--- angular-softuni-forum/client/src/app/services/themes.service.ts
+++ angular-softuni-forum/client/src/app/services/themes.service.ts
@@ -1,13 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Theme } from '../types';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ThemesService {
-  constructor(private httpClient: HttpClient) {}
+  constructor(private http: HttpClient) {}
+
+  private apiUrl = 'http://localhost:3000/api/themes';
 
   getThemes() {
-    //
+    return this.http.get<Theme[]>(this.apiUrl);
   }
 }
