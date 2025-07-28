88b3ff335ca23537e9666bb87ae5950930cf3380
Boris Todorov <btodorovtodorov@gmail.com>
2025-07-28 17:01:31 +0300

    Update themes-list.component.ts

--- angular-softuni-forum/client/src/app/components/themes-list/themes-list.component.ts
+++ angular-softuni-forum/client/src/app/components/themes-list/themes-list.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ThemesService } from '../../services';
 
 @Component({
   selector: 'app-themes-list',
   imports: [],
   templateUrl: './themes-list.component.html',
-  styleUrl: './themes-list.component.css'
+  styleUrl: './themes-list.component.css',
 })
 export class ThemesListComponent {
+  constructor(private api: ThemesService) {}
 
+  ngOnInit(): void {
+    this.api.getThemes().subscribe((themes) => {
+      console.log(themes);
+    });
+  }
 }
