e87ef59b1f829a8ae908c9676e5eb4aaed0a2480
Boris Todorov <btodorovtodorov@gmail.com>
2025-08-04 14:44:08 +0300

    show/hide buttons for loggedin/notloggedin users

--- angular-softuni-forum/client/src/app/components/core/header/header.component.ts
+++ angular-softuni-forum/client/src/app/components/core/header/header.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../../services';
 
 @Component({
   selector: 'app-header',
@@ -9,5 +10,14 @@ import { RouterLink } from '@angular/router';
   standalone: true,
 })
 export class HeaderComponent {
-  isLoggedIn = false;
+  protected authService = inject(AuthService);
+  private router = inject(Router);
+
+  readonly isLoggedIn = this.authService.isLoggedIn;
+  readonly currentUser = this.authService.currentUser;
+
+  logout(): void {
+    this.authService.logout();
+    this.router.navigate(['/home']);
+  }
 }
