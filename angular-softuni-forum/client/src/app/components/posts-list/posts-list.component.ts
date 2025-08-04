aa3abb880d60e8f0d2aba81d3c37c5040218caa8
Boris Todorov <btodorovtodorov@gmail.com>
2025-08-04 17:39:25 +0300

    update post component

--- angular-softuni-forum/client/src/app/components/posts-list/posts-list.component.ts
+++ angular-softuni-forum/client/src/app/components/posts-list/posts-list.component.ts
@@ -2,10 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { PostsService } from '../../services';
 import { Post } from '../../types';
 import { LoaderComponent } from '../shared/loader/loader.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-posts-list',
-  imports: [LoaderComponent],
+  imports: [LoaderComponent, RouterLink],
   templateUrl: './posts-list.component.html',
   styleUrl: './posts-list.component.css',
 })
--- angular-softuni-forum/client/src/app/components/themes-list/themes-list.component.ts
+++ angular-softuni-forum/client/src/app/components/themes-list/themes-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { AuthService, ThemesService } from '../../services';
-import { Theme } from '../../types';
+import { AuthService, PostsService, ThemesService } from '../../services';
+import { Post, Theme } from '../../types';
 import { LoaderComponent } from '../shared/loader/loader.component';
 import { Observable } from 'rxjs';
 import { RouterLink } from '@angular/router';
@@ -15,14 +15,19 @@ import { PostsListComponent } from '../posts-list/posts-list.component';
 export class ThemesListComponent implements OnInit {
   isLoading: boolean = true;
 
-  // themes$: Observable<Theme[]>;
+  posts$: Observable<Post[]>;
+  themes$: Observable<Theme[]>;
   themes: Theme[] = [];
 
   private authService = inject(AuthService);
   readonly isLoggedIn = this.authService.isLoggedIn;
 
-  constructor(private themeService: ThemesService) {
-    // this.themes$ = this.themeService.getThemes();
+  constructor(
+    private themeService: ThemesService,
+    private postService: PostsService
+  ) {
+    this.themes$ = this.themeService.getThemes();
+    this.posts$ = this.postService.getPosts();
   }
 
   ngOnInit(): void {
